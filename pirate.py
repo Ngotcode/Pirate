a49fe74d501484c8fcc1fd6b4e5d63b69c114d0c
Ngotcode <ngot@janelia.hhmi.org>
2016-07-18 12:35:11 -0400

    write text file

--- pirate.py
+++ pirate.py
@@ -4,3 +4,10 @@ print 'Arrg'
 
 for i in range(1000):
     print i
+
+print "i love pasta"
+
+with open('temp.txt', 'w') as fh:
+    for i in range(5):
+        fh.write( "walk three paces left")
+        
